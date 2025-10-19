74e3aeffb772ed96451811979a31fa31b1212bdf
Syed Ammar Hashmi <153057772+chocogititup@users.noreply.github.com>
2025-10-19 23:27:34 +0530

    Add files via upload
    Latest Commit

---
+++ src/test/java/cv/MyCv.java
@@ -0,0 +1,17 @@
+package cv;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class MyCv {
+    private String name;
+    private int age;
+    private String address;
+    private String email;
+    private String phone;
+    private String education;
+    private String skills;
+    private String experience;
+}
